000e4add8017f3ce7f1acede100a9d6e2f1e30f4
nenoNaninu <neno@neno.dev>
2020-01-27 00:48:03 +0900

    update: AggressiveInlining

--- Unity/UniWebSocket/Assets/UniWebSocket/Validations/ValidationUtils.cs
+++ Unity/UniWebSocket/Assets/UniWebSocket/Validations/ValidationUtils.cs
@@ -1,20 +1,23 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using UniWebSocket.Exceptions;
 
 namespace UniWebSocket.Validations
 {
     internal static class ValidationUtils
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool IsNullOrEmpty(this Array array)
         {
             return (array == null || array.Length == 0);
         }
-        
+
         /// <summary>
         /// It throws <exception cref="WebSocketBadInputException"></exception> if value is null or empty/white spaces
         /// </summary>
         /// <param name="value">The value to be validated</param>
         /// <param name="name">Input parameter name</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateInput(string value, string name)
         {
             if (string.IsNullOrWhiteSpace(value))
@@ -28,6 +31,7 @@ namespace UniWebSocket.Validations
         /// </summary>
         /// <param name="value">The value to be validated</param>
         /// <param name="name">Input parameter name</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateInput(byte[] value, string name)
         {
             if (value.IsNullOrEmpty())
@@ -41,6 +45,7 @@ namespace UniWebSocket.Validations
         /// </summary>
         /// <param name="value">The value to be validated</param>
         /// <param name="name">Input parameter name</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateInput<T>(T value, string name)
         {
             if (Equals(value, default(T)))
